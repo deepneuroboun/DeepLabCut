13458f7c77e727a4e48bcde758ec8e48bda355ff
Safa Andac <8544658+SanD94@users.noreply.github.com>
2020-04-08 14:35:26 +0300

    change layout for welcome page, adds bounneuro image

--- deeplabcut/gui/welcome.py
+++ deeplabcut/gui/welcome.py
@@ -16,8 +16,10 @@ import deeplabcut
 
 media_path = os.path.join(deeplabcut.__path__[0], 'gui' , 'media')
 dlc = os.path.join(media_path,'dlc_1-01.png')
+bounneuro = os.path.join(media_path, 'bounneuro.png')
 
 class Welcome(wx.Panel):
+    SCALE = 5
 
     def __init__(self, parent,gui_size):
         wx.Panel.__init__(self, parent)
@@ -26,18 +28,33 @@ class Welcome(wx.Panel):
         wx.Panel.__init__(self, parent, -1,style=wx.SUNKEN_BORDER,size=(h,w))
 
 ##         design the panel
-        sizer = wx.GridBagSizer(5, 5)
-        # Add image of DLC
-        icon = wx.StaticBitmap(self, bitmap=wx.Bitmap(dlc))
-        sizer.Add(icon, pos=(0, 0), span=(0, 8),flag=wx.EXPAND|wx.BOTTOM, border=10)
-        line = wx.StaticLine(self)
-        sizer.Add(line, pos=(1, 0), span=(1, 8),flag=wx.EXPAND|wx.BOTTOM, border=10)
-
+        main_sizer = wx.BoxSizer(wx.VERTICAL)
         #if editing this text make sure you add the '\n' to get the new line. The sizer is unable to format lines correctly.
-        description = "DeepLabCut™ is an open source tool for markerless\n pose estimation of user-defined body parts with deep learning.\nA. and M.W. Mathis Labs | http://www.deeplabcut.org\n\nWelcome to the DeepLabCut Project GUI!\nTo get started, please click on the 'Manage Project'\n tab to create or load an existing project."
+        description = "Allah belanizi versin cocuklar!\nBaciniz yok!!!\nBakalim istedigimiz sekilde mi oldu???"
 
         self.proj_name = wx.StaticText(self, label=description,style=wx.ALIGN_CENTRE)
-        sizer.Add(self.proj_name, pos=(2, 3), border=10)
-        sizer.AddGrowableCol(2)
-        self.SetSizer(sizer)
-        sizer.Fit(self)
+        main_sizer.Add(self.proj_name, proportion=1, flag=wx.EXPAND, border=10)
+
+
+
+        # Add image of DLC
+        icon_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        icon_image = wx.Image(dlc, type=wx.BITMAP_TYPE_PNG)
+        boun_image = wx.Image(bounneuro, type=wx.BITMAP_TYPE_PNG)
+
+        icon = wx.StaticBitmap(self, bitmap=wx.Bitmap(icon_image.Scale(icon_image.GetWidth() // self.SCALE, icon_image.GetHeight() // self.SCALE, wx.IMAGE_QUALITY_HIGH)))
+        boun = wx.StaticBitmap(self, bitmap=wx.Bitmap(boun_image.Scale(boun_image.GetWidth() // self.SCALE, boun_image.GetHeight() // self.SCALE, wx.IMAGE_QUALITY_HIGH)))
+
+        icon_sizer.Add(boun, proportion=1, flag=wx.EXPAND)
+        icon_sizer.Add(icon, proportion=0, flag=wx.ALIGN_RIGHT | wx.EXPAND)
+
+        main_sizer.Add(icon_sizer, proportion=0, flag=wx.EXPAND, border=10)
+
+
+
+
+
+        self.SetSizer(main_sizer)
+        main_sizer.Fit(self)
+
+
